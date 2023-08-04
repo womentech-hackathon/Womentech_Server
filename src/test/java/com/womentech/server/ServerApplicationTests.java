e045a2752e1b738f1f23ac5a290df17e5626f5c0
soyeonii <dingdingcrong@naver.com>
2023-08-04 13:34:39 +0900

    fix: Fix error

--- src/test/java/com/womentech/server/ServerApplicationTests.java
+++ src/test/java/com/womentech/server/ServerApplicationTests.java
@@ -2,7 +2,11 @@ package com.womentech.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@ContextConfiguration(classes = ServerApplicationTests.class)
 @SpringBootTest
 class ServerApplicationTests {
 
